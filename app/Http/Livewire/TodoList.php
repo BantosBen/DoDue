6db68776959517af33b9f1aae35bf9955d5a9536
Angatia Benson <angatiabenson1@gmail.con>
2023-08-10 04:34:07 -0500

    feat: LiveWire component setup, livewire upgrade

--- app/Http/Livewire/TodoList.php
+++ app/Http/Livewire/TodoList.php
@@ -6,6 +6,12 @@ use Livewire\Component;
 
 class TodoList extends Component
 {
+    public $name;
+
+    public function create()
+    {
+        dd("create");
+    }
     public function render()
     {
         return view('livewire.todo-list');
